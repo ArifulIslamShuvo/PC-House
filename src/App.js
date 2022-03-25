28d7910390eb17d9a99f6951cfb0286ba6d6d5a2
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-03-25 22:54:27 +0600

    start PC-House priject

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>P<span>C</span> House</h2>
+    </div>
+  );
+}
+
+export default App;
