eb1f15a66204982377a5c1136887bdacf2e28262
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-03-26 20:46:05 +0600

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ function App() {
       <h2 className='name'>P<span>C</span> House</h2>
       <Shop></Shop>
       <div className='question-ans'>
-        <h3>Props Vs State</h3>
+      <h3>How React Work:</h3>
+        <p>A JavaScript library response for building user interfaces. Feedback Document Object Model Dom keeps a separate copy called virtualDom. And it works faster than the real dom. The response simply compares the real dom with the virtual dom. And with the help of algorithms, we decide where our markup has changed. Renders only the thing that has changed the element without rendering the entire dom and shows the distance</p>
+        
+        <h3>Props Vs State:</h3>
         <h5>State:</h5>
-        <p>State is a dynamic storage that can be used to store data inside our components. State changes can be asynchronous. Contains information about state variables and state elements. It cannot be accessed by child material.</p>
+        <p>The state is dynamic storage that can be used to store or store data inside our components. State changes can be asynchronous. Contains information about state variables and state elements. It cannot be accessed by child material.</p>
         <h5>Props</h5>
-        <p>Props read only. Props are immutable. It lets data move from one element to another. Props are immutable so we can't change props from inside the material</p>
+        <p>Props are the set of attributes that are passed from a parent component to a child component. These can be data variables or functions.
 
-        <h3>How React Work</h3>
-        <p>The React Document Object Model Dom keeps a separate copy. One called VirtualDom, which has a real dom, works fast ৷ The response simply compares the real dom with the virtual world. And the algorithm decides where our markup has changed. The only thing that has changed is that the entire dom has been changed without checking.</p>
+          Props are primarily used to communicate between parent and child components. Moreover, props cannot be altered inside the child component Props read only. Props are immutable. It lets data move from one element to another. Props are immutable so we can't change props from inside the material</p>
       </div>
     </div>
   );
